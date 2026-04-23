9246bfc321d7e143c37e827a8382ee060521b723
Adisai Plomlee <adisai.p@innovationssolution.com>
2026-04-23 11:40:16 +0700

    Add views for approving return weight requests and updating certificates
    - Created IndexApproveReturnWeight.cshtml to manage return weight approval requests with filtering options and a responsive table layout.
    - Implemented IndexUpdateCertificate.cshtml for handling certificate update requests, including search filters and status management.

--- obj/Debug/net9.0/AspnetCoreMvcFull.AssemblyInfo.cs
+++ obj/Debug/net9.0/AspnetCoreMvcFull.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a61207c03bb8e527a8e94b7baf5a016a19be2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe4c781afccb4831ce604b5fd2568ce0b92223a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
